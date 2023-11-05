8ecf642292c04df0b5745eba40bd129cc3be7654
Viraj Raut <virajraut089@gmail.com>
2023-11-05 11:42:10 +0530

    this is simple program to create a rect component

---
+++ react-component/src/App.jsx
@@ -0,0 +1,11 @@
+import CustomButton from "./CustomButton"
+
+function App() {
+  return (
+    <CustomButton></CustomButton>
+  )
+
+
+}
+
+export default App
---
+++ react-component/src/CustomButton.jsx
@@ -0,0 +1,12 @@
+
+function CustomButton() {
+
+    return (
+        <div>
+            <h1>This is custom button</h1>
+            <button>click custom buton</button>
+        </div>
+    )
+}
+
+export default CustomButton;
